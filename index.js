8fe093cb1da580a0f9f72b7670192a0903261f02
Bimo Bramantyo <bim.bramantyo@gmail.com>
2018-02-07 15:40:40 +0700

    passport setup

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+
 const PORT = process.env.PORT || 5000
+
 const app = express();
+
+passport.use(new GoogleStrategy());
+
 
 app.get('/', (req, res) => {
   res.send({hi: 'there'})
